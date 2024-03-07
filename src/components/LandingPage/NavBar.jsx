186a4b40746ddaa4e489a66523a78f05818737cd
Naresh <chandanbatven@gmail.com>
2024-03-08 01:28:59 +0530

    image bug
    Signed-off-by: Naresh <chandanbatven@gmail.com>

--- src/components/LandingPage/NavBar.jsx
+++ src/components/LandingPage/NavBar.jsx
@@ -5,7 +5,10 @@ function Navbar({ toggleNav }) {
   return (
     <div className="fixed top-0 z-50 backdrop-blur-mdl grid grid-flow-col w-screen h-[7.38vh] bg-[rgb(19,19,19)] bg-opacity-[74%] items-center">
       <div className="flex flex-row items-center h-fit">
-        <img src="/assets/logo2.png" className="h-[7vw] w-[35vw] m-4"></img>
+        <img
+          src="https://github.com/Naresh-chandanbatve/decor/blob/main/src/assets/logo2.png"
+          className="h-[7vw] w-[35vw] m-4"
+        ></img>
       </div>
 
       <HamburgerIcon
